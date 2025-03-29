b429950a26833a6b7012250d902fe7f4631e2518
samtiji <samtzyxx@gmail.com>
2025-03-30 04:26:42 +0700

    Create anjaykk.js

---
+++ anjaykk.js
@@ -0,0 +1,9 @@
+{
+  "secretCode": "FUCKYOU",
+  "description": "ANJAY ALOK", 
+  "lastUpdated": "KAPAN KAPAN",
+  "author": "Samzy", 
+  "status": "Active",
+  "license": "MIT",
+  "version": "1.0.0"
+}
